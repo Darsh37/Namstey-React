fc360abbe6acdb585f95e02bfdb093587a23efe8
darshana Shingade <darshanahingade.gmail.com>
2023-03-31 18:21:32 +0530

    dev_first_commit

---
+++ App.js
@@ -0,0 +1,15 @@
+const heading =
+React.createElement("div",{ id:"parents"},
+[React.createElement("div",{ id:"child"},
+[React.createElement("h1",{ id:"parents"},"This is my H1 tag"),
+React.createElement("h2",{ id:"parents"},"This is my H2 tag")]),
+
+React.createElement("div",{ id:"child"},
+[React.createElement("h1",{ id:"parents"},"This is my H1 tag"),
+React.createElement("h2",{ id:"parents"},"This is my H2 tag")])
+]);
+
+const root = ReactDOM.createRoot
+(document.getElementById("root"));
+
+root.render(heading); 
