41c62577abe4515b789f0a2f6f1010aa4f1c6b0d
darshana Shingade <darshanahingade.gmail.com>
2023-04-14 20:44:48 +0530

    Removed all files that are in the .gitignore

--- App.js
+++ App.js
@@ -2,17 +2,41 @@ import React from 'react';
 import ReactDOM from "react-dom/client" 
 
 
-const heading =
-React.createElement("div",{ id:"parents"},
-[React.createElement("div",{ id:"child"},
-[React.createElement("h1",{ id:"parents"},"This is my H1 tag"),
-React.createElement("h2",{ id:"parents"},"This is my H2 tag")]),
+// const heading =
+// React.createElement("div",{ id:"parents"},
+// [React.createElement("div",{ id:"child"},
+// [React.createElement("h1",{ id:"parents"},"This is my H1 tag"),
+// React.createElement("h2",{ id:"parents"},"This is my H2 tag")]),
 
-React.createElement("div",{ id:"child"},
-[React.createElement("h1",{ id:"parents"},"This is my H1 tag"),
-React.createElement("h2",{ id:"parents"},"This is my H2 tag")])
-]);
+// React.createElement("div",{ id:"child"},
+// [React.createElement("h1",{ id:"parents"},"This is my H1 tag"),
+// React.createElement("h2",{ id:"parents"},"This is my H2 tag")])
+// ]);
+
+const Heading =()=>(<h1 className='head'tabIndex={5}>Namste React using JSX</h1>);
+
+const HeadingComponent =()=>(
+<div>
+<h1 className='head'>Namstey React Functional Component fdjkgkfdmglk</h1>
+</div>
+)  ;
+
+const number=1000;
+const elem =<span>React element</span>     
+
+
+
+const HeadingComponent2 = ()=>(
+    <div className='container'>
+        {elem }
+        <Heading/>
+        <HeadingComponent/>
+        {HeadingComponent()}
+       <h1 className='heading'>Nastey React using function Component</h1> 
+    </div>
+
+)
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(heading); 
+root.render(<HeadingComponent2/>,<HeadingComponent/>); 
