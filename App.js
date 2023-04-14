e0a5315f3cc453df23057bf2c7e1ec78993ae01e
darshana Shingade <darshanahingade.gmail.com>
2023-04-14 13:52:24 +0530

    First commit

--- App.js
+++ App.js
@@ -1,3 +1,7 @@
+import React from 'react';
+import ReactDOM from "react-dom/client" 
+
+
 const heading =
 React.createElement("div",{ id:"parents"},
 [React.createElement("div",{ id:"child"},
@@ -9,7 +13,6 @@ React.createElement("div",{ id:"child"},
 React.createElement("h2",{ id:"parents"},"This is my H2 tag")])
 ]);
 
-const root = ReactDOM.createRoot
-(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(heading); 
